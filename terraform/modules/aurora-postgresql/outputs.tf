29708a0ebaa9493ec4258148c8ef97ceff3382b7
edgarrth <edgarrt@gmail.com>
2026-07-06 07:58:05 -0500

    reestructurando carpetas

--- terraform/modules/aurora-postgresql/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "endpoint" { value = aws_rds_cluster.this.endpoint }
-output "reader_endpoint" { value = aws_rds_cluster.this.reader_endpoint }
-output "port" { value = aws_rds_cluster.this.port }
-output "security_group_id" { value = aws_security_group.this.id }
-output "secret_value_json" {
-  value     = jsonencode({ username = var.master_username, password = random_password.master.result, host = aws_rds_cluster.this.endpoint, reader_host = aws_rds_cluster.this.reader_endpoint, port = aws_rds_cluster.this.port, database = var.database_name })
-  sensitive = true
-}
--- terraform/modules/waf/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" { type = string }
-variable "scope" {
-  type    = string
-  default = "REGIONAL"
-}
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
